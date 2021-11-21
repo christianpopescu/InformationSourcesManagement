fd6d50c438b35d10f6752fd595b9cc0da122ba24
Christian Popescu <christian.popescu@outlook.com>
2021-11-21 17:22:57 +0100

    Test Select file with Swing and print first page for the selected page

--- InformationSourcesManagement/src/main/java/fr/vadc/java/ISManagement.java
+++ InformationSourcesManagement/src/main/java/fr/vadc/java/ISManagement.java
@@ -1,6 +1,9 @@
 package fr.vadc.java;
 
 import fr.vadc.java.Sandbox.TestPdf;
+import fr.vadc.java.Sandbox.TestSwing;
+
+import java.io.File;
 
 /**
  * Information Sources management
@@ -11,6 +14,12 @@ public class ISManagement
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        TestPdf.printPage("F:\\CCP_library\\Doc\\epo\\Data Mining with Decision Trees.pdf", 0);
+        File selectedFile = TestSwing.chooseFile();
+        //TestPdf.printPage("F:\\CCP_library\\Doc\\epo\\Data Mining with Decision Trees.pdf", 0);
+
+        if (selectedFile != null)
+            TestPdf.printPage(selectedFile,0);
+        else
+            System.out.println("File not selected");
     }
 }
