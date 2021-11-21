596da25aa4351cc4d915f83bf05233cfc5399252
Christian Popescu <christian.popescu@outlook.com>
2021-11-21 15:39:42 +0100

    Refactoring file name and page number set as parameters

--- InformationSourcesManagement/src/main/java/fr/vadc/java/ISManagement.java
+++ InformationSourcesManagement/src/main/java/fr/vadc/java/ISManagement.java
@@ -11,6 +11,6 @@ public class ISManagement
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        TestPdf.testPrintFirstPdfPage();
+        TestPdf.printPage("F:\\CCP_library\\Doc\\epo\\Data Mining with Decision Trees.pdf", 0);
     }
 }
--- InformationSourcesManagement/src/main/java/fr/vadc/java/Sandbox/TestPdf.java
+++ InformationSourcesManagement/src/main/java/fr/vadc/java/Sandbox/TestPdf.java
@@ -1,32 +1,32 @@
 package fr.vadc.java.Sandbox;
 
-import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.rendering.ImageType;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.apache.pdfbox.tools.imageio.ImageIOUtil;
 
-import java.io.*;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 public class TestPdf {
 
     public static void testPrintFirstPdfPage() {
         String pdfFilename = "F:\\CCP_library\\Doc\\epo\\Data Mining with Decision Trees.pdf";
-        try {                               // /f/CCP_library/Doc/epo/Data Mining with Decision Trees.pdf
-            PDDocument document = PDDocument.load(new File(pdfFilename));
-            // }
-        PDFRenderer pdfRenderer = new PDFRenderer(document);
-        for (int page = 0; page < 1 /*document.getNumberOfPages()*/; ++page) {
+        printPage(pdfFilename, 1);
+    }
+
+    public static void printPage(String pPdfFilename, int pPage) {
+        try {
+            PDDocument document = PDDocument.load(new File(pPdfFilename));
+            PDFRenderer pdfRenderer = new PDFRenderer(document);
             try {
-                BufferedImage bim = pdfRenderer.renderImageWithDPI(page, 300, ImageType.RGB);
+                BufferedImage bim = pdfRenderer.renderImageWithDPI(pPage, 300, ImageType.RGB);
                 // suffix in filename will be used as the file format
-                ImageIOUtil.writeImage(bim, pdfFilename + "-" + (page + 1) + ".png", 300);
-            } catch(IOException ex) {
+                ImageIOUtil.writeImage(bim, pPdfFilename + "-" + (pPage + 1) + ".png", 300);
+            } catch (IOException ex) {
                 //ignore
             }
-
-
-        }
             document.close();
         } catch (IOException ex) {
             System.out.println(ex.getMessage());
