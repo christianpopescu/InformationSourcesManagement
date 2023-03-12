56706f5b9d9131d26e92dda4c22eb2449e4b341f
Christian Popescu <christian.popescu@outlook.com>
2023-03-12 23:41:07 +0100

    start tu implement recursive research + apply action

--- CSharp/src/ISManagement/ISManagementDomain/FileAndFolder/FileAndFolderManager.cs
+++ CSharp/src/ISManagement/ISManagementDomain/FileAndFolder/FileAndFolderManager.cs
@@ -8,5 +8,33 @@ namespace ISManagementDomain.FileAndFolder
 {
     internal class FileAndFolderManager
     {
+
+        /// <summary>
+        /// 
+        /// Recursive enumerates files in directory, filtered with a given model and apply action 
+        /// </summary>
+        /// <param name="pDirectory"></param>
+        /// <param name="pFunctionToApply"></param>
+        /// <param name="pFilePattern"></param>
+        public void EnumerateFileApplyAction(string pDirectory, Action<string> pFunctionToApply, string pFilePattern = "*.*")
+        {
+            if (!Directory.Exists(pDirectory)) return;
+
+            // 1. Process files from directory
+            var collectionOfFiles = Directory.EnumerateFiles(pDirectory, pFilePattern, SearchOption.AllDirectories);
+            foreach (var file in collectionOfFiles)
+            {
+                pFunctionToApply(file);
+            }
+            
+            // 2. For each subdirectory recursive process
+
+            // todo enumerate directory
+        }
+
+        private void RecusiveEnumerateAndApplyAction(string pDirectory, Action<string> pFunctionToApply, string pFilePattern)
+        {
+            // todo write recusive function
+        }
     }
 }
