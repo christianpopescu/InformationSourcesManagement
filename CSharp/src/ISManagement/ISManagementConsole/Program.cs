e321e4bb38f1ca562cbcf0f95f6438762c3561a4
Christian Popescu <christian.popescu@outlook.com>
2023-03-14 23:05:14 +0100

    Add console application + test enumerate file and folders

---
+++ CSharp/src/ISManagement/ISManagementConsole/Program.cs
@@ -0,0 +1,19 @@
+﻿using ISManagementDomain.FileAndFolder;
+namespace ISManagementConsole
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            FileAndFolderManager ffm = new FileAndFolderManager();
+            ffm.EnumerateFileApplyAction(@"D:\temp", WriteFileNameOnConsole);
+
+        }
+
+        static void WriteFileNameOnConsole(string fileName)
+        {
+            Console.WriteLine(Path.GetFileName(fileName));
+        }
+    }
+}
--- CSharp/src/ISManagement/ISManagementDomain/FileAndFolder/FileAndFolderManager.cs
+++ CSharp/src/ISManagement/ISManagementDomain/FileAndFolder/FileAndFolderManager.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace ISManagementDomain.FileAndFolder
 {
-    internal class FileAndFolderManager
+    public class FileAndFolderManager
     {
 
         /// <summary>
