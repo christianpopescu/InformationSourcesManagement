642bf3c76ad8d9c3e8358476222c2a21547b8b51
Christian Popescu <christian.popescu@outlook.com>
2023-03-15 23:53:37 +0100

    change function

--- CSharp/src/ISManagement/ISManagementConsole/Program.cs
+++ CSharp/src/ISManagement/ISManagementConsole/Program.cs
@@ -3,17 +3,19 @@ namespace ISManagementConsole
 {
     internal class Program
     {
+        protected static List<string> foldersList= new List<string>();
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
             FileAndFolderManager ffm = new FileAndFolderManager();
-            ffm.EnumerateFileApplyAction(@"D:\temp", WriteFileNameOnConsole);
-
+            ffm.EnumerateFileApplyAction(@"D:\Temp\TempToDelete\sandboxkm.wiki", WriteFileNameOnConsole);
+            foreach(string folder in foldersList) Console.WriteLine(folder);
         }
 
         static void WriteFileNameOnConsole(string fileName)
         {
             Console.WriteLine(Path.GetFileName(fileName));
+            foldersList.Add(Path.GetDirectoryName(fileName)??"");
         }
     }
 }
