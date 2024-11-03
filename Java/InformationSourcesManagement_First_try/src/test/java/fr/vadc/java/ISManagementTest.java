d6eb2ef7439a9ebb21214240fcccbefd393bbfa1
Christian Popescu <christian.popescu@outlook.com>
2024-11-03 18:08:20 +0100

    Java version - doc added

---
+++ Java/InformationSourcesManagement_First_try/src/test/java/fr/vadc/java/ISManagementTest.java
@@ -0,0 +1,20 @@
+package fr.vadc.java;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ISManagementTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
