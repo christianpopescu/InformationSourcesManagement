9b72db6665886bbcfd37ac19471114893c35268c
Christian Popescu <christian.popescu@outlook.com>
2024-11-04 10:36:36 +0100

    Menu added to the main form

--- Java/src/ISM_Java/UI_Swing/src/main/java/fr/vadc/forms/MainForm.java
+++ Java/src/ISM_Java/UI_Swing/src/main/java/fr/vadc/forms/MainForm.java
@@ -1,25 +1,41 @@
 package fr.vadc.forms;
-import java.awt.event.*;
-import javax.swing.*;
 
-public class MainForm extends JFrame
-{
+import javax.swing.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+public class MainForm extends JFrame {
     private static final int DEFAULT_WIDTH = 600;
     private static final int DEFAULT_HEIGHT = 400;
 
-    private JLabel label = new JLabel();
-    private JPanel panel = new JPanel();
-public MainForm()
-    {
+    public MainForm() {
+        intialize();
         setSize(DEFAULT_WIDTH, DEFAULT_HEIGHT);
-        addWindowListener(new WindowAdapter( ) {
+        addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
+    }
+    
+    protected void intialize()
+    {
+        JPanel panel;
+        JLabel label = new JLabel();
         label.setText("Information Source Management");
+        panel = new JPanel();
         panel.add(label);
         add(panel);
+        JMenu mainMenu = new JMenu("Main Menu");
+        JMenu firstSubMenu = new JMenu("First Sub Menu");
+        JMenuItem miShowImage = new JMenuItem("Show Image");
+        firstSubMenu.add(miShowImage);
+        mainMenu.add(miShowImage);
+        add(mainMenu);
+        JMenuBar mb=new JMenuBar();
+        mb.add(mainMenu);
+        setJMenuBar(mb);
+
     }
 }
