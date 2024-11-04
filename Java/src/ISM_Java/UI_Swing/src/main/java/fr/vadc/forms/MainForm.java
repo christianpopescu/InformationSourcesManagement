b1fb7c7774ebd1dde7abe7334ab8a09f5c5f6a24
Christian Popescu <christian.popescu@outlook.com>
2024-11-04 14:50:39 +0100

    [Java] Action on menu triggered

--- Java/src/ISM_Java/UI_Swing/src/main/java/fr/vadc/forms/MainForm.java
+++ Java/src/ISM_Java/UI_Swing/src/main/java/fr/vadc/forms/MainForm.java
@@ -1,12 +1,20 @@
 package fr.vadc.forms;
 
 import javax.swing.*;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainForm extends JFrame {
+import static javax.swing.JOptionPane.showMessageDialog;
+
+public class MainForm extends JFrame implements ActionListener {
     private static final int DEFAULT_WIDTH = 600;
     private static final int DEFAULT_HEIGHT = 400;
+    private JMenuItem miShowImage;
+    JMenu mainMenu;
+    JMenu firstSubMenu;
 
     public MainForm() {
         intialize();
@@ -18,18 +26,26 @@ public class MainForm extends JFrame {
             }
         });
     }
-    
-    protected void intialize()
-    {
+
+    public void actionPerformed(ActionEvent ev) {
+        if (ev.getSource() == this.miShowImage) {
+            showMessageDialog(null, "ShowImage menuitem selected");
+        } else {
+            showMessageDialog(null, ev.getSource().getClass().getName());
+        }
+    }
+
+    protected void intialize()    {
         JPanel panel;
         JLabel label = new JLabel();
         label.setText("Information Source Management");
         panel = new JPanel();
         panel.add(label);
         add(panel);
-        JMenu mainMenu = new JMenu("Main Menu");
-        JMenu firstSubMenu = new JMenu("First Sub Menu");
-        JMenuItem miShowImage = new JMenuItem("Show Image");
+        mainMenu = new JMenu("Main Menu");
+        firstSubMenu = new JMenu("First Sub Menu");
+        miShowImage = new JMenuItem("Show Image");
+        miShowImage.addActionListener(this);
         firstSubMenu.add(miShowImage);
         mainMenu.add(miShowImage);
         add(mainMenu);
