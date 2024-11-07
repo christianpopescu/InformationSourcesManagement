f20ea3d07cd49ebfe9f1c4ac14c366b056363b45
Christian Popescu <christian.popescu@outlook.com>
2024-11-08 00:09:03 +0100

    Java - Enumerate Files and Directories in a directory

--- Java/src/ISM_Java/UI_Console/src/main/java/fr/vadc/App.java
+++ Java/src/ISM_Java/UI_Console/src/main/java/fr/vadc/App.java
@@ -1,4 +1,7 @@
 package fr.vadc;
+import java.io.*;
+import java.nio.file.*;
+import java.nio.file.attribute.*;
 
 /**
  * Hello world!
@@ -6,5 +9,29 @@ package fr.vadc;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String dirName ="E:\\CCP_Library_New\\IT\\ComputerScience\\Programming Languages\\Languages\\Java\\Core";
+
+        try ( DirectoryStream<Path> dirstrm =
+                      Files.newDirectoryStream(Path.of(dirName)) )
+        {
+            System.out.println("Directory of " + dirName);
+// Because DirectoryStream implements Iterable, we
+// can use a "foreach" loop to display the directory.
+            for(Path entry : dirstrm) {
+                BasicFileAttributes attribs =
+                        Files.readAttributes(entry, BasicFileAttributes.class);
+                if(attribs.isDirectory())
+                    System.out.print("<DIR> ");
+                else
+                    System.out.print("         ");
+                System.out.println(entry.toAbsolutePath().toString());
+            }
+        } catch(InvalidPathException e) {
+            System.out.println("Path Error " + e);
+        } catch(NotDirectoryException e) {
+            System.out.println(dirName + " is not a directory.");
+        } catch (IOException e) {
+            System.out.println("I/O Error: " + e);
+        }
     }
 }
