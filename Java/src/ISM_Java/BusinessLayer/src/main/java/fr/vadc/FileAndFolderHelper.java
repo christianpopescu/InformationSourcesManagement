d52f75c1920169450d3b11134b64bc1b671e0c61
Christian Popescu <christian.popescu@outlook.com>
2024-11-10 23:48:29 +0100

    Modify file and folder iterator

--- Java/src/ISM_Java/BusinessLayer/src/main/java/fr/vadc/FileAndFolderHelper.java
+++ Java/src/ISM_Java/BusinessLayer/src/main/java/fr/vadc/FileAndFolderHelper.java
@@ -4,4 +4,8 @@ package fr.vadc;
  * Class to manage the search and iteration through files and folders
  */
 public class FileAndFolderHelper {
+	public static void RecursiveIterateFolders(Path root)
+	{
+		// TODO - implementation
+	}
 }
